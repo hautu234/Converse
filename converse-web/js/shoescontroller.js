b9e8a6b739d6e77e5c7d07c9ad54c4b7690dbbb8
Thanh Huy Duo <thanhhuy.duo@gmail.com>
2015-11-30 14:59:52 +0700

    dynamic binding

--- converse-web/js/shoescontroller.js
+++ converse-web/js/shoescontroller.js
@@ -16,125 +16,27 @@ var app = angular.module('converseBMT', ['ngRoute'])
 
 .controller('shoesController', ['$scope','$routeParams', '$location', '$http', function($scope, $routeParams, $location, $http) {
     var shoes = [
-        {
-            "name": "Nike Air Max",
-            "price": 350000,
-            "color": "Hồng",
-            "material": "Vải (Poly)",
-            "imageUrl":"https://farm6.staticflickr.com/5808/23174288811_33f6fc9782_o.jpg"
-        },
-        {
-            "name": "Nike Air Test",
-            "price": 120000,
-            "color": "Hồng",
-            "material": "Vải (Poly)",
-            "imageUrl":"https://farm1.staticflickr.com/714/22744219537_87d3a514b0_o.jpg"
-        },
-        {
-            "name": "Nike Air Max",
-            "price": 350000,
-            "color": "Hồng",
-            "material": "Vải (Poly)",
-            "imageUrl":"https://farm6.staticflickr.com/5808/23174288811_33f6fc9782_o.jpg"
-        },
-        {
-            "name": "Nike Air Test",
-            "price": 120000,
-            "color": "Hồng",
-            "material": "Vải (Poly)",
-            "imageUrl":"https://farm1.staticflickr.com/714/22744219537_87d3a514b0_o.jpg"
-        },
-        {
-            "name": "Nike Air Max",
-            "price": 350000,
-            "color": "Hồng",
-            "material": "Vải (Poly)",
-            "imageUrl":"https://farm6.staticflickr.com/5808/23174288811_33f6fc9782_o.jpg"
-        },
-        {
-            "name": "Nike Air Test",
-            "price": 120000,
-            "color": "Hồng",
-            "material": "Vải (Poly)",
-            "imageUrl":"https://farm1.staticflickr.com/714/22744219537_87d3a514b0_o.jpg"
-        },
-        {
-            "name": "Nike Air Max",
-            "price": 350000,
-            "color": "Hồng",
-            "material": "Vải (Poly)",
-            "imageUrl":"https://farm6.staticflickr.com/5808/23174288811_33f6fc9782_o.jpg"
-        },
-        {
-            "name": "Nike Air Test",
-            "price": 120000,
-            "color": "Hồng",
-            "material": "Vải (Poly)",
-            "imageUrl":"https://farm1.staticflickr.com/714/22744219537_87d3a514b0_o.jpg"
-        },
-        {
-            "name": "Nike Air Test",
-            "price": 120000,
-            "color": "Hồng",
-            "material": "Vải (Poly)",
-            "imageUrl":"https://farm1.staticflickr.com/714/22744219537_87d3a514b0_o.jpg"
-        },
-        {
-            "name": "Nike Air Max",
-            "price": 350000,
-            "color": "Hồng",
-            "material": "Vải (Poly)",
-            "imageUrl":"https://farm6.staticflickr.com/5808/23174288811_33f6fc9782_o.jpg"
-        },
-        {
-            "name": "Nike Air Test",
-            "price": 120000,
-            "color": "Hồng",
-            "material": "Vải (Poly)",
-            "imageUrl":"https://farm1.staticflickr.com/714/22744219537_87d3a514b0_o.jpg"
-        },
-        {
-            "name": "Nike Air Max",
-            "price": 350000,
-            "color": "Hồng",
-            "material": "Vải (Poly)",
-            "imageUrl":"https://farm6.staticflickr.com/5808/23174288811_33f6fc9782_o.jpg"
-        },
-        {
-            "name": "Nike Air Test",
-            "price": 120000,
-            "color": "Hồng",
-            "material": "Vải (Poly)",
-            "imageUrl":"https://farm1.staticflickr.com/714/22744219537_87d3a514b0_o.jpg"
-        },
-        {
-            "name": "Nike Air Test",
-            "price": 120000,
-            "color": "Hồng",
-            "material": "Vải (Poly)",
-            "imageUrl":"https://farm1.staticflickr.com/714/22744219537_87d3a514b0_o.jpg"
-        }
+
     ];
 
-    var displayShoes = [];
-    var temp = [];
-    for(i = 0; i < shoes.length; i++) {
-        if(i % 4 == 0 || i  == shoes.length - 1 ) {
-            displayShoes.push(temp);
+    var brand = $location.search().brand;
+    if(brand) {
+        $http.get('/data/' + brand + '.json').then(function(response) {
+            shoes = response.data.items;
+            $scope.type = response.data.type;
+
+            var displayShoes = [];
             var temp = [];
-        }
-        temp.push(shoes[i]);
+            for(i = 0; i < shoes.length; i++) {
+                temp.push(shoes[i]);
+                if(temp.length == 4 || i  == shoes.length - 1 ) {
+                    displayShoes.push(temp);
+                    var temp = [];
+                }
+            }
+
+            $scope.displayShoes = displayShoes;
+        });
     }
-
-    console.log("params" + JSON.stringify($location.search().test));
-
-    $http.get('/data/nike.json').then(function(response) {
-        console.log(response);
-        $scope.shoesdata = response;
-    });
-
-    console.log("huy" + $scope.shoesdata);
-
-    $scope.displayShoes = displayShoes;
 }]);
 
