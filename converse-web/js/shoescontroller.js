dff1ddc9ac2aa8e8ca83140daaa0148606fda2b0
Thanh Huy Duo <thanhhuy.duo@gmail.com>
2015-12-01 16:56:56 +0700

    refactor projects using directive

--- converse-web/js/shoescontroller.js
+++
@@ -1,8 +0,0 @@
-var app = angular.module('converseBMT', ['ngRoute'])
-
-.controller('shoesController', ['$scope','$routeParams', '$location', '$http', function($scope, $routeParams, $location, $http) {
-    $scope.$on('$includeContentLoaded', function(event) {
-        angular.element('.pgwSlideshow').pgwSlideshow();
-    });
-}]);
-
