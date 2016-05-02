2188fcdf3972c65666590249e5a318ae7b36d86d
Ubuntu <ubuntu@ip-172-31-20-146.us-west-2.compute.internal>
2016-05-02 08:22:38 +0000

    modify bash

--- converse-web/deploy.sh
+++ converse-web/deploy.sh
@@ -3,7 +3,7 @@ echo "starting killing nodejs processes"
 killall -9 nodejs
 echo "finished killing nodejs"
 echo "start killing off mongod"
-killall -9 mongod
+sudo service  mongod stop
 echo "start checking repository"
 git status && git fetch
 echo "switching to master"
@@ -11,12 +11,11 @@ git checkout master
 echo "pull the latest code"
 git pull
 echo "start mongod"
-mongod 
+sudo service mongod start 
 echo "start nodejs"
 forever start app.js &
-echo "kill off  apache2"
-killall -9 apache2
+echo "kill off apache2"
+sudo service apache2 stop
 echo "start apache2"
-service start apache2
-
+sudo service apache2 start
 echo "finish deployment"
